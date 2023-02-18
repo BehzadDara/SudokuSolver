e16187195be0aeafe4b672f6cf56ba77a8c39d7b
behzad dara <Behzad.dara.99@gmail.com>
2023-02-18 17:53:52 +0330

    Add project files.

---
+++ SudokuSolver/Problem.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    public static class Problem
+    {
+        #region Init
+
+        public static int[][] Solve(int[][] sudoku)
+        {
+            #region Begin
+            Console.WriteLine($"Answer of the sudoku is:");
+            #endregion
+
+            #region Solve
+            var result = FindSolution(sudoku, Tuple.Create(0, 0));
+            #endregion
+
+            #region End
+            PrintResult(result);
+            return result;
+            #endregion
+        }
+
+        #endregion
+
+        #region Solution
+
+        private static int[][] FindSolution(int[][] sudoku, Tuple<int, int> position)
+        {
+            if (IsAnswer(sudoku))
+                return sudoku;
+
+            if (sudoku[position.Item1][position.Item2] == 0)
+            {
+                var k = 1;
+
+                while (k <= 9)
+                {
+                    if (IsSafeChoice(sudoku, position, k))
+                    {
+                        sudoku[position.Item1][position.Item2] = k;
+
+                        var result = FindSolution(sudoku, NextPosition(position));
+                        if (IsAnswer(result))
+                            return result;
+
+                        sudoku[position.Item1][position.Item2] = 0;
+                    }
+
+                    k++;
+                }
+
+                return sudoku;
+            }
+            else
+            {
+                return FindSolution(sudoku, NextPosition(position));
+            }
+
+        }
+
+        private static Tuple<int, int> NextPosition(Tuple<int, int> position)
+        {
+            var i = position.Item1;
+            var j = position.Item2;
+
+            if (i == 8)
+            {
+                if (j == 8)
+                {
+                    throw new Exception();
+                }
+
+                j++;
+                i = 0;
+
+            }
+            else
+            {
+                i++;
+            }
+
+            return Tuple.Create(i, j);
+        }
+
+        private static bool IsSafeChoice(int[][] sudoku, Tuple<int, int> position, int k)
+        {
+            var i = position.Item1;
+            var j = position.Item2;
+
+            #region check the row
+            if (sudoku[i].Contains(k))
+                return false;
+            #endregion
+
+            #region check the column
+            foreach (var row in sudoku)
+            {
+                if (row[j] == k)
+                    return false;
+            }
+            #endregion
+
+            #region check the square
+            var baseI = i / 3;
+            var baseJ = j / 3;
+            for (var p = 0; p < 3; p++)
+            {
+                for (var q = 0; q < 3; q++)
+                {
+                    if (sudoku[3 * baseI + p][3 * baseJ + q] == k)
+                        return false;
+                }
+            }
+            #endregion
+
+            return true;
+        }
+
+        private static bool IsAnswer(int[][] sudoku)
+        {
+            foreach(var row in sudoku)
+            {
+                if (row.Contains(0))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        private static void PrintResult(int[][] result)
+        {
+            for(var i = 0; i < 9; i++)
+            {
+                for(var j = 0; j < 9; j++)
+                {
+                    Console.Write($"{result[i][j]}  ");
+                    if(j == 2 || j == 5)
+                    {
+                        Console.Write("|  ");
+                    }
+                }
+                Console.WriteLine();
+                if(i == 2 || i == 5)
+                {
+                    Console.WriteLine("--------------------------------");
+                }
+            }
+        }
+    }
+}
---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,35 @@
+﻿#region Problem
+/*
+You are given a Sudoku grid with some empty cells. 
+You need to write a code such that all the cells are filled in a proper fashion according to Sudoku rules which are:
+    Each of the digits 1-9 must occur exactly once in each row.
+    Each of the digits 1-9 must occur exactly once in each column.
+    Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+*/
+#endregion
+
+#region Solution
+
+namespace SudokuSolver;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[][] sudoku = {
+            new int[] { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+            new int[] { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+            new int[] { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+            new int[] { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+            new int[] { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+            new int[] { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+            new int[] { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+            new int[] { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+            new int[] { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+        };
+
+        Problem.Solve(sudoku);
+    }
+}
+
+#endregion
